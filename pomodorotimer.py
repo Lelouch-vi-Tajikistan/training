9714d9bcce32591d7ca6e88a020860ba28648551
Perdeuj <muminovamir@gmail.com>
2023-04-25 19:55:16 +0200

    Modified the code using pynput library

--- pomodorotimer.py
+++ pomodorotimer.py
@@ -1,17 +1,44 @@
 import time
 from os import system
+from pynput import keyboard
+
+exit_flag = False
+
+def on_press(key):
+    global exit_flag
+    try:
+        if key.char == 'q':
+            exit_flag = True
+    except AttributeError:
+        pass
 
 def pomodoro_timer(work_duration=25, break_duration=5, intervals=4):
+    global exit_flag
+    listener = keyboard.Listener(on_press=on_press)
+    listener.start()
+
     for interval in range(1, intervals + 1):
         print(f"Interval {interval}/{intervals}")
-        print(f"Work for {work_duration} minutes.")
+        print(f"Work for {work_duration} minutes. Press 'q' to stop the timer.")
         for _ in range(work_duration * 60):
+            if exit_flag:
+                break
             time.sleep(1)
-        print(f"Take a {break_duration} minute break.")
-        for _ in range(break_duration * 60):
-            time.sleep(1)
+        if exit_flag:
+            break
 
-    print("Congratulations! You've completed all intervals.")
+        print(f"Take a {break_duration} minute break. Press 'q' to stop the timer.")
+        for _ in range(break_duration * 60):
+            if exit_flag:
+                break
+            time.sleep(1)
+        if exit_flag:
+            break
+
+    if not exit_flag:
+        print("Congratulations! You've completed all intervals.")
+    else:
+        print("Pomodoro timer stopped.")
 
 def main():
     work_duration = int(input("Enter the work duration in minutes (default: 25): ") or 25)
